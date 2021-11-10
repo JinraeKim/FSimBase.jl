a5a13d303fb53dfd67d4575cc0e7b3b7a3aea99c
JinraeKim <kjl950403@gmail.com>
2021-11-11 00:02:23 +0900

    wip

--- src/FSimBase.jl
+++ src/FSimBase.jl
@@ -14,7 +14,7 @@ using DataFrames
 
 export AbstractEnv, State, Params, Dynamics, Dynamics!
 export AbstractController, Command
-export sim, apply_inputs
+export apply_inputs
 export Simulator
 export solve, reinit!, log!, step!, step_until!
 
